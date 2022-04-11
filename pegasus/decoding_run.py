bbbb53829c071729fa0d0f4b48d641cb37792599
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-04-11 15:42:06 +1000

    Fixed missing comma

--- pegasus/decoding_run.py
+++ pegasus/decoding_run.py
@@ -112,7 +112,7 @@ if __name__ == '__main__':
     
     training_dataset, test_dataset = generate_data(
         tokenizer, device, sample_min, sample_max,
-        n_training_examples, n_test_examples, 'Decoding'
+        n_training_examples, n_test_examples, 'Decoding',
         use_word_format=args.use_words)
     
     training_dataloader = DataLoader(training_dataset, 
