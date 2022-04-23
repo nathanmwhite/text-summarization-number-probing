dfe8523ac9ec9ce15affb04bb733a659753068a6
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-04-23 18:41:58 +1000

    Created UnitsModel for model.py

--- pegasus/model.py
+++ pegasus/model.py
@@ -198,3 +198,40 @@ class AdditionModel(torch.nn.Module):
         y_pred = self.sequential(embeddings_concat).squeeze(-1)
 
         return y_pred
+
+# TODO: also sentence units model
+class UnitsModel(torch.nn.Module):
+    def __init__(self, embedding_model, output_dim):
+        super(UnitsModel, self).__init__()
+
+        self.embedding_model = embedding_model
+
+        encoder = self.embedding_model.model.encoder
+        input_dim = encoder.layer_norm.normalized_shape[0] * 2
+        hidden_dim = 50
+
+        # they fail to specify their hidden_dim anywhere
+        self.linear_1 = torch.nn.Linear(in_features=input_dim,
+                                        out_features=hidden_dim)
+        self.linear_2 = torch.nn.Linear(in_features=hidden_dim,
+                                        out_features=hidden_dim)
+        self.linear_3 = torch.nn.Linear(in_features=hidden_dim,
+                                        out_features=output_dim)
+        self.sequential = torch.nn.Sequential(self.linear_1,
+                                              torch.nn.ReLU(),
+                                              self.linear_2,
+                                              torch.nn.ReLU(),
+                                              self.linear_3)
+
+    # TODO: test dimensionality of the following
+    def forward(self, input_text):
+        forward = self.embedding_model.model.forward(**input_text)
+        encoder_state = forward.encoder_last_hidden_state
+
+        embeddings = encoder_state.detach()[:, :-1]
+
+        embeddings_concat = torch.flatten(embeddings, start_dim=1)
+
+        y_pred = self.sequential(embeddings_concat).squeeze(-1)
+
+        return y_pred
