890f7a135ff86ed15f24a0c4e1424171f67fc94f
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-05-06 15:02:48 +1000

    Update decoding_run.py

--- pegasus/decoding_run.py
+++ pegasus/decoding_run.py
@@ -38,7 +38,7 @@ def train_epoch(idx, training_data_loader, model, loss_function, optimizer):
         outputs = model(inputs)
         
         # testing only
-        print('Inputs size:', inputs.size())
+        print('Inputs size:', inputs['input_ids'].size())
         print('Outputs size:', outputs.size())
         print('Labels size:', labels.size())
         
