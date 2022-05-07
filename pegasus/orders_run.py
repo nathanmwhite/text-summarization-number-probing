ecbc8c75b9420d708626db622dbe03b2b85afe2f
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-05-07 17:46:37 +1000

    Update orders_run.py

--- pegasus/orders_run.py
+++ pegasus/orders_run.py
@@ -144,7 +144,7 @@ if __name__ == '__main__':
         training_batch_size = 64
     
     training_dataloader = DataLoader(training_dataset, 
-                                     batch_size=64, 
+                                     batch_size=training_batch_size, 
                                      shuffle=True)
     test_dataloader = DataLoader(test_dataset, 
                                  batch_size=1, 
