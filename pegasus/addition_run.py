bb0f09746d5def101c44ded47ecbdfb573d01123
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-05-10 16:21:09 +1000

    Update addition_run.py

--- pegasus/addition_run.py
+++ pegasus/addition_run.py
@@ -227,7 +227,7 @@ if __name__ == '__main__':
                       args.epochs,
                       args.trial_number)
                       
-    message = f"Model hyperparameters:" + ' | '.join(str(w) for w in hyperparam_set)
+    message = f"Model hyperparameters: " + ' | '.join(str(w) for w in hyperparam_set)
     report_phase(message)
     message = f"Test RMSE: {rmse}"
     report_phase(message)
