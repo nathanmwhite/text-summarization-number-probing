0d6fed8ebfd68fad52ba88fb1d4c86f997e5f708
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-05-10 12:05:45 +1000

    Update decoding_run.py

--- pegasus/decoding_run.py
+++ pegasus/decoding_run.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
     embedding_model = get_embedding_model(model_name)
         
     if args.freeze_embedder:
-        freeze_module(embedding_model, 'Pegasus')
+        freeze_module(embedding_model, args.embedding_model)
     embedding_model = embedding_model.to(device)
     
     dm = DecodingModel(embedding_model, padded_seq_len).to(device)
