9a352d204aa862faedd62af0e25ce021d5e8d938
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-05-11 12:19:22 +1000

    Update decoding_run.py

--- pegasus/decoding_run.py
+++ pegasus/decoding_run.py
@@ -95,6 +95,7 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', type=int, default=10)
     parser.add_argument('--freeze_embedder', type=bool, default=False)
     parser.add_argument('--log_filename', type=str, default='decoding_number.log')
+    parser.add_argument('--trial_number', type=int, default=1)
     args = parser.parse_args()
     
     check_arguments(args)
@@ -117,8 +118,8 @@ if __name__ == '__main__':
     n_training_examples = args.training_examples
     n_test_examples = args.test_examples
     
-    phase_message = 'Begin generating dataset.'
-    report_phase(phase_message)
+#     phase_message = 'Begin generating dataset.'
+#     report_phase(phase_message)
     
     training_dataset, test_dataset = generate_data(
         tokenizer, device, sample_min, sample_max,
@@ -147,8 +148,8 @@ if __name__ == '__main__':
                                  batch_size=1, 
                                  shuffle=True)
     
-    phase_message = 'Completed generating dataset.'
-    report_phase(phase_message)
+#     phase_message = 'Completed generating dataset.'
+#     report_phase(phase_message)
     
     embedding_model = get_embedding_model(model_name)
         
@@ -158,8 +159,8 @@ if __name__ == '__main__':
     
     dm = DecodingModel(embedding_model, padded_seq_len).to(device)
 
-    phase_message = 'Model set up.'
-    report_phase(phase_message)
+#     phase_message = 'Model set up.'
+#     report_phase(phase_message)
     
     loss_fn = torch.nn.MSELoss()
     
@@ -175,14 +176,14 @@ if __name__ == '__main__':
     
     EPOCHS = args.epochs
     
-    phase_message = 'Begin training.'
-    report_phase(phase_message)
+#     phase_message = 'Begin training.'
+#     report_phase(phase_message)
     
     epoch_number = 0
     
     for epoch in range(EPOCHS):
-        epoch_message = 'Begin epoch {n}'.format(n=epoch_number + 1)
-        report_phase(epoch_message)
+#         epoch_message = 'Begin epoch {n}'.format(n=epoch_number + 1)
+#         report_phase(epoch_message)
 
         # Make sure gradient tracking is on, and do a pass over the data
         dm.train(True)
@@ -191,26 +192,37 @@ if __name__ == '__main__':
                                                             loss_fn, 
                                                             optimizer)
         
-        phase_message = f"End of epoch average batch loss: {avg_loss}"
-        report_phase(phase_message)
-        phase_message = f"End of epoch last loss: {continuing_loss}"
-        report_phase(phase_message)
-        phase_message = f"Epoch total loss: {total_loss}"
-        report_phase(phase_message)
+#         phase_message = f"End of epoch average batch loss: {avg_loss}"
+#         report_phase(phase_message)
+#         phase_message = f"End of epoch last loss: {continuing_loss}"
+#         report_phase(phase_message)
+#         phase_message = f"Epoch total loss: {total_loss}"
+#         report_phase(phase_message)
         
         epoch_number += 1
         
     # temporary: save last version of model
     # TODO: reimplement to save best version
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    model_path = f"model_{timestamp}_{epoch_number}"
-    torch.save(dm.state_dict(), model_path)
+#     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+#     model_path = f"model_{timestamp}_{epoch_number}"
+#     torch.save(dm.state_dict(), model_path)
         
     # testing and metrics
-    message = 'Training finished.'
+#     message = 'Training finished.'
+#     report_phase(message)
+#     message = 'Begin evaluation.'
+#     report_phase(message)
+    hyperparam_set = ('Decoding trial',
+                      args.embedding_model,
+                      args.training_examples,
+                      args.lr,
+                      args.momentum,
+                      args.epochs,
+                      args.trial_number)
+                      
+    message = f"Model hyperparameters: " + ' | '.join(str(w) for w in hyperparam_set)
     report_phase(message)
-    message = 'Begin evaluation.'
-    report_phase(message)
+
     mse = evaluate(dm, loss_fn, test_dataloader)
     rmse = math.sqrt(mse)
     message = f"Test RMSE: {rmse}"
