50757ba340f4a59c64a76092e3ffc3e5ff4abd81
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-05-31 12:47:19 +1000

    Update addition_run.py

--- pegasus/addition_run.py
+++ pegasus/addition_run.py
@@ -23,6 +23,7 @@ from torch.utils.data import DataLoader
 from .generate_data import generate_data
 from .model import AdditionModel, report_phase, freeze_module
 from .util import check_arguments, get_model_name, get_tokenizer, get_embedding_model
+from .early_stopping import Early_Stopping
 
 
 def train_epoch(idx, training_data_loader, model, loss_function, optimizer):
@@ -100,6 +101,8 @@ if __name__ == '__main__':
     parser.add_argument('--log_filename', type=str, default='addition.log')
     parser.add_argument('--trial_number', type=int, default=1)
     parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--early_stopping', type=bool, default=False)
+    parser.add_argument('--patience', type=int, default=10)
     args = parser.parse_args()
     
     check_arguments(args)
@@ -185,6 +188,8 @@ if __name__ == '__main__':
     
 #     phase_message = 'Begin training.'
 #     report_phase(phase_message)
+
+    early_stopping = Early_Stopping(min_delta=0.0, patience=30)
     
     epoch_number = 0
     
@@ -204,6 +209,14 @@ if __name__ == '__main__':
 #         report_phase(phase_message)
 #         phase_message = f"Epoch total loss: {total_loss}"
 #         report_phase(phase_message)
+
+        if args.early_stopping:
+            early_stopping(total_loss)
+            if early_stopping.early_stopping == True:
+                message = f'Early stopping of training at epoch {epoch_number}.'
+                report_phase(message)
+                break
+                
         
         epoch_number += 1
         
