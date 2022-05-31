716934be21e03927e38ffc2749b361633cbab86d
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-05-31 12:52:55 +1000

    Update ranges_run.py

--- pegasus/ranges_run.py
+++ pegasus/ranges_run.py
@@ -22,6 +22,7 @@ from torch.utils.data import DataLoader
 from .generate_data import generate_data
 from .model import RangeModel, report_phase, freeze_module
 from .util import check_arguments, get_model_name, get_tokenizer, get_embedding_model
+from .early_stopping import Early_Stopping
 
 
 class SiameseMSELoss(torch.nn.Module):
@@ -116,6 +117,8 @@ if __name__ == '__main__':
     parser.add_argument('--log_filename', type=str, default='decoding_ranges.log')
     parser.add_argument('--trial_number', type=int, default=1)
     parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--early_stopping', type=bool, default=False)
+    parser.add_argument('--patience', type=int, default=10)
     args = parser.parse_args()
     
     check_arguments(args)
@@ -200,6 +203,8 @@ if __name__ == '__main__':
     
 #     phase_message = 'Begin training.'
 #     report_phase(phase_message)
+
+    early_stopping = Early_Stopping(min_delta=0.0, patience=args.patience)
     
     epoch_number = 0
     
@@ -219,6 +224,13 @@ if __name__ == '__main__':
 #         report_phase(phase_message)
 #         phase_message = f"Epoch total loss: {total_loss}"
 #         report_phase(phase_message)
+
+        if args.early_stopping:
+            early_stopping(total_loss)
+            if early_stopping.early_stopping == True:
+                message = f'Early stopping of training at epoch {epoch_number}.'
+                report_phase(message)
+                break
         
         epoch_number += 1
         
