6978fb6d6254b9c1a39033e969291429d5b198e7
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-06-07 11:15:03 +1000

    Update decoding_run.py

--- pegasus/decoding_run.py
+++ pegasus/decoding_run.py
@@ -129,7 +129,8 @@ if __name__ == '__main__':
         tokenizer, device, sample_min, sample_max,
         n_training_examples, n_test_examples, 'Decoding',
         use_word_format=args.use_words, 
-        simple_iteration=args.simple_iteration)
+        simple_iteration=args.simple_iteration,
+        float_=args.float)
     
     if args.embedding_model in ('Pegasus', 'T5', 'SSR', 'ProphetNet'):
         start_token_length = 0
