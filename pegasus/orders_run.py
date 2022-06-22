4d66edb61c93dceeba1f41116f8f63031208a03b
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-06-23 09:16:05 +1000

    Update orders_run.py

--- pegasus/orders_run.py
+++ pegasus/orders_run.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     parser.add_argument('--patience', type=int, default=10)
     parser.add_argument('--clip_norm', type=int, default=5)
     parser.add_argument('--trained', action='store_true')
-    parser.add_argument('--no-trained', dest='trained', action='store_false')
+    parser.add_argument('--untrained', dest='trained', action='store_false')
     parser.set_defaults(trained=True)
     args = parser.parse_args()
     
