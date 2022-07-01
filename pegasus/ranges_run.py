c55da44890b6a4acb248fa394111f08472a7f438
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-07-01 13:15:10 +1000

    Update ranges_run.py

--- pegasus/ranges_run.py
+++ pegasus/ranges_run.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
     parser.add_argument('--early_stopping', type=bool, default=False)
     parser.add_argument('--patience', type=int, default=10)
     parser.add_argument('--clip_norm', type=int, default=5)
-    parser.add_argument('--hidden_dim', type=int, default=100)
+    parser.add_argument('--hidden_dim', type=int, default=50)
     parser.add_argument('--trained', action='store_true')
     parser.add_argument('--untrained', dest='trained', action='store_false')
     parser.set_defaults(trained=True)
