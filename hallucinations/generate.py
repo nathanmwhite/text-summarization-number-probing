033b9f6c74c769d1cbae89e350692d41ed2e7077
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-08-21 13:02:32 +1000

    Update generate.py

--- hallucinations/generate.py
+++ hallucinations/generate.py
@@ -8,35 +8,47 @@ __copyright__ = "Copyright © 2022 Nathan M. White"
 __author__ = "Nathan M. White"
 __author_email__ = "nathan.white1@jcu.edu.au"
 
+import argparse
+
 import torch
 
 from datasets import load_dataset
 
-from transformers import PegasusTokenizer, PegasusForConditionalGeneration
+#from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 
-tokenizer = PegasusTokenizer.from_pretrained('google/pegasus-xsum')
-model = PegasusForConditionalGeneration.from_pretrained('google/pegasus-xsum')
+from .util import get_model_name, get_tokenizer, get_embedding_model
 
-results = []
-for item in dataset['test']:
-    doc = item['document']
-    tokenized = tokenizer(doc, return_tensors='pt', truncation=True, max_length=128)
-    decoder_tokenized = tokenizer('<s>', return_tensors='pt')
-    tokenized['decoder_input_ids'] = decoder_tokenized['input_ids']
-    last_word = None
-    while True: # TODO: replace with maximum output length allowed by model decoder
-        if last_word is not None:
-            # TODO: determine correct reference to output dimensionality and update
-            tokenized['decoder_input_ids'] = torch.cat([tokenized['decoder_input_ids'], last_word])
-        output = model(**tokenized)
-        last_word = output[-1]
-        if last_word == '<eos>': # TODO: replace with correct call for '<eos>' index
-            break
-    # TODO: implement conversion from final decoder_input_ids
-    out_sequence = decoder_output_to_word(tokenized['decoder_input_ids'])
-    results.append((doc, out_sequence))
+
+def generate_results(tokenizer, model, dataset):
+    results = []
+    for item in dataset['test']:
+        doc = item['document']
+        #tokenized = tokenizer(doc, return_tensors='pt', truncation=True, max_length=128)
+        batch_result = tokenizer.prepare_seq2seq_batch(src_texts=doc)
+        out = model.generate(**batch_result)
+        out_sequence = tokenizer.batch_decode(out)
+        results.append((doc, out_sequence))
+    return results
+
+
+def record_results(results):
+    for (a, b) in results:
+        print(a)
+        print('-->')
+        print(b)
+        print('-----')
+
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--embedding_model', type=str, default='Pegasus')
+    args = parser.parse_args()
+
+    model_name = get_model_name(args.embedding_model)
+    tokenizer = get_tokenizer(model_name)
+    model = get_embedding_model(model_name)
     
-for (a, b) in results:
-    print(a)
-    print('-->')
-    print(b)
+    dataset = load_dataset('xsum', cache_dir='./models')
+
+    results = generate_results(tokenizer, model, dataset)
+    record_results(results)
