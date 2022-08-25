64eb5ca9688059e59c84ea0b78230c635548e53d
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-08-25 22:28:12 +1000

    Update addition_run.py

--- pegasus/addition_run.py
+++ pegasus/addition_run.py
@@ -140,9 +140,9 @@ if __name__ == '__main__':
         n_training_examples, n_test_examples, 'Addition',
         use_word_format=args.use_words, float_=args.float)
     
-    if args.embedding_model in ('Pegasus', 'T5', 'SSR', 'ProphetNet'):
+    if args.embedding_model in ('Pegasus', 'T5', 'T5-CDM', 'SSR', 'ProphetNet', 'ProphetNet-CDM'):
         start_token_length = 0
-    elif args.embedding_model in ('Bert', 'Bart', 'DistilBart', 'UniLM', 'Random'):
+    elif args.embedding_model in ('Bert', 'Bart', 'Bart-XSum', 'DistilBart', 'UniLM', 'Random'):
         start_token_length = 1
 #     else:
 #         raise ValueError('Error: --embedding_model must be a valid model type.')
