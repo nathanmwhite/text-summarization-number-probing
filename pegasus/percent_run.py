1d4fcc850aaca150ec7a35d047fe5f74731514a6
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-08-26 09:21:23 +1000

    Update percent_run.py

--- pegasus/percent_run.py
+++ pegasus/percent_run.py
@@ -144,9 +144,9 @@ if __name__ == '__main__':
         simple_iteration=args.simple_iteration,
         float_=args.float)
     
-    if args.embedding_model in ('Pegasus', 'T5', 'SSR', 'ProphetNet'):
+    if args.embedding_model in ('Pegasus', 'T5', 'T5-CDM', 'SSR', 'ProphetNet', 'ProphetNet-CDM'):
         start_token_length = 0
-    elif args.embedding_model in ('Bart', 'DistilBart', 'UniLM', 'Bert', 'Random'):
+    elif args.embedding_model in ('Bart', 'Bart-XSum', 'DistilBart', 'UniLM', 'Bert', 'Random'):
         start_token_length = 1
 #     else:
 #         raise ValueError('Error: --embedding_model must be a valid model type.')
