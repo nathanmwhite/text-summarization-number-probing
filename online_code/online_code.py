d783d148ef27c9aba07e30bc58ea9edbd8fb132d
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2022-10-29 07:25:04 +1000

    Update online_code.py

--- online_code/online_code.py
+++ online_code/online_code.py
@@ -12,6 +12,9 @@ class OnlineCode:
         self._log_sum += self._t_1 * np.log2(self._K)
 
     def update_with_results(self, outputs, labels):
+        # TODO: fix next line
+        # np.prod, not np.product
+        # TODO: review approach here
         prod = np.product(outputs[labels.astype(bool)])
         self._log_sum += (-1 * np.log2(prod))
 
@@ -22,5 +25,15 @@ class OnlineCode:
         # confirm expected value for joint just q(x, y) is
         #  the prequential codelength, or something further
         e_q = get_prequential_codelength()
+        # TODO: Blier and Ollivier (2018: 6) on Online Code:
+        #  "a model with default values is used to encode the first few data"
+        #  "such a model may learn from the first k data samples"
+        #  "a uniform encoding is used for the first few points"
+        #  This suggests that a model with default values (assumed here, untrained model)
+        #   is the "uniform encoding"
+        #  This currently uses a naive calculation roughly based on Voita and Titov, p. 3:
+        #   "Compression is usually compared against uniform encoding which does not require
+        #     any learning from data. It assumes p(y|x) = p_unif(y|x) = 1/K, and yields
+        #     codelength L_unif(y_1:n|x_1:n) = n log_2 K bits"
         uniform_encoding = n * np.log2(self._K)
         return uniform_encoding - codelength
