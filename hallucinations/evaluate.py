22eaefbcd469bd922ad21265a5550974c0d2dc2f
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2023-01-12 09:27:29 +1000

    Create evaluate.py

---
+++ hallucinations/evaluate.py
@@ -0,0 +1,40 @@
+# Text summarization number probing
+# Original code Copyright © 2023 Nathan M. White
+
+# Copyright notice
+__copyright__ = "Copyright © 2023 Nathan M. White"
+
+# author
+__author__ = "Nathan M. White"
+__author_email__ = "nathan.white1@jcu.edu.au"
+
+from transformers import PegasusForConditionalGeneration 
+from transformers import T5ForConditionalGeneration
+from transformers import BartForConditionalGeneration
+from transformers import ProphetNetForConditionalGeneration
+
+from ..pegasus.util import get_tokenizer, get_embedding_model
+
+# TODO: create support for evaluating on the final version of malo_cleaned
+
+from torch.utils.data import Dataset, DataLoader
+
+
+class GenerationDataset(Dataset):
+    def __init__(self, input_data):
+        self._input_ids = input_data['input_ids']
+        self._attention_mask = input_data['attention_mask']
+        
+    def __len__(self):
+        return len(self._input_ids)
+    
+    def __getitem__(self, index):
+        ids = self._input_ids[index]
+        mask = self._attention_mask[index]
+        datapoint = {'input_ids': ids, 'attention_mask': mask}
+        
+        return datapoint
+    
+    def to(self, device):
+        self._input_ids.to(device)
+        self._attention_mask.to(device)
