ccce2b0dc53e34f87bfdac663b328cbb32d66bc9
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2023-01-12 09:29:35 +1000

    added is_a_number for hallucination

--- hallucinations/util.py
+++ hallucinations/util.py
@@ -7,3 +7,25 @@ __copyright__ = "Copyright © 2023 Nathan M. White"
 # author
 __author__ = "Nathan M. White"
 __author_email__ = "nathan.white1@jcu.edu.au"
+
+def is_a_number(thing, include_hyphen=False):
+    if include_hyphen:
+        symbols = (',', '-', '.')
+    else:
+        symbols = (',', '.')
+        
+    if thing == '':
+        return False
+    elif thing[0].isdigit() and thing[-1].isdigit():
+        for c in thing[1:-1]:
+            if c.isdigit() or c in symbols:
+                continue
+            else:
+                return False
+        return True
+    elif thing in ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']:
+        return True
+    else:
+        return False
+      
+      
