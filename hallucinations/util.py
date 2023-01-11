42bc9a07366230748c618151fabb8eb248a3c65c
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2023-01-12 09:34:42 +1000

    wrote documentation for is_a_number

--- hallucinations/util.py
+++ hallucinations/util.py
@@ -8,22 +8,33 @@ __copyright__ = "Copyright © 2023 Nathan M. White"
 __author__ = "Nathan M. White"
 __author_email__ = "nathan.white1@jcu.edu.au"
 
-def is_a_number(thing, include_hyphen=False):
+def is_a_number(sequence, include_hyphen=False):
+    """
+    is_a_number : checks if a character sequence is a number, possibly containing
+        a thousands comma or a decimal point, or is a number between one and ten
+        as a string. This function allows the hallucination evaluator to find the
+        set of numbers in input and output to compare.
+    @param sequence (str) : a string containing the sequence to check if a number
+    @param include_hyphen (bool) : whether to allow hyphens when recognizing a
+         number, such as with negative numbers or ranges.
+    returns : boolean representing whether the sequence is a number given the
+        definition here
+    """
     if include_hyphen:
         symbols = (',', '-', '.')
     else:
         symbols = (',', '.')
         
-    if thing == '':
+    if sequence == '':
         return False
-    elif thing[0].isdigit() and thing[-1].isdigit():
-        for c in thing[1:-1]:
+    elif sequence[0].isdigit() and sequence[-1].isdigit():
+        for c in sequence[1:-1]:
             if c.isdigit() or c in symbols:
                 continue
             else:
                 return False
         return True
-    elif thing in ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']:
+    elif sequence in ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']:
         return True
     else:
         return False
