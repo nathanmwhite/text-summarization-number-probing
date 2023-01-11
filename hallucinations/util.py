07aa84f619a3aa4c302792c9f8440ed886437653
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2023-01-12 09:28:47 +1000

    Create util.py

---
+++ hallucinations/util.py
@@ -0,0 +1,9 @@
+# Text summarization number probing
+# Original code Copyright © 2023 Nathan M. White
+
+# Copyright notice
+__copyright__ = "Copyright © 2023 Nathan M. White"
+
+# author
+__author__ = "Nathan M. White"
+__author_email__ = "nathan.white1@jcu.edu.au"
