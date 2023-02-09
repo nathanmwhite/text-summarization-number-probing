9cbc2adc0aaa0986b524c0c32291492dee5d1d05
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2023-02-09 13:35:10 +1000

    Update evaluate.py

--- hallucinations/evaluate.py
+++ hallucinations/evaluate.py
@@ -8,6 +8,8 @@ __copyright__ = "Copyright © 2023 Nathan M. White"
 __author__ = "Nathan M. White"
 __author_email__ = "nathan.white1@jcu.edu.au"
 
+import argparse
+
 import logging
 
 from transformers import PegasusForConditionalGeneration 
